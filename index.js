218007b4804d4c0fc0431e2927ac5473871ac53e
Steven <59514341+SteveNT69@users.noreply.github.com>
2020-10-10 20:37:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const shoot_imgs = [
-'https://media1.tenor.com/images/77c313e3e1e0b31f5cc5f66a4f7c7217/tenor.gif?itemid=11101357',
-'https://lparchive.org/Grand-Theft-Auto-San-Andreas-(Screenshot)/Update%2021/11-gtasa11.gif',
-'https://thumbs.gfycat.com/FoolhardyPositiveGlowworm-max-1mb.gif',
-]
 
 client.on("ready", () => {
   console.log("Ready to go mothafuckas");
-  client.user.setPresence({ activity: { name: 'Plague Inc.' }, status: 'dnd' })
+  client.user.setPresence({ activity: { name: 'I\'m back bitches' }, status: 'dnd' })
 })
 
 client.on("message", (message) => {
@@ -19,7 +14,7 @@ client.on("message", (message) => {
     message.channel.send({embed: {
     color: 3447003,
     title: "Command List",
-    description: "This is a list of all the commands I have. The command prefix is `hm!`.",
+    description: "This is a list of all the commands I have. The command prefix is `hm!`.  All commands must be typed in lower case.",
     fields: [{
         name: "Ping",
         value: "See my ping time in ms."
@@ -37,6 +32,10 @@ client.on("message", (message) => {
         value: "Vibe to the British anthem like a true knighted gamer."
       },
       {
+        name: "Time",
+        value: "Shows the current time."
+      },
+      {
         name: "Shoot @someone",
         value: "Shoot your enemy, perhaps a Soviet spy or a drunk Irishman?"
       },
@@ -44,9 +43,21 @@ client.on("message", (message) => {
         name: "Cum",
         value: "Every moment is a consume the cum chalice moment!"
       },
+      {
+        name: "RuleTag",
+        value: "Yes, even I, the Queen, have a RuleTag from RuleConnect34! Make your own RuleTag [here](https://rc34.xyz/ruletag.html)."
+      },
+      {
+        name: "六四事件",
+        value: "了解有关六四事件的真相！"
+      },
+      {
+        name: "Bugger @someone",
+        value: "Bugger someone the Bruno Powroznik way, bareback!"
+      },
     ],
     footer: {
-      text: "This bot was made by confusedNT#6501"
+      text: "This bot was made by confusedNT#6969."
 	}
   }
 });
@@ -64,6 +75,10 @@ client.on("message", (message) => {
     message.channel.send("Never gonna give you up, never gonna let you down... https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif");
   } else
 
+  if (message.content.startsWith(config.prefix + "time")) {
+    message.channel.send("The current time is **69:00**.");
+  }
+
   if (message.content.startsWith(config.prefix + "anthem")) {
     message.channel.send("God is saving me so I'll never die bitch https://youtu.be/szfHgbIj65k");
   }
@@ -80,6 +95,23 @@ let member = message.mentions.members.first()
   if (message.content.startsWith(config.prefix + "cum")) {
     message.channel.send("I, Queen Elizabeth the Second, humbly submit a toast! https://media.discordapp.net/attachments/662398947716890675/700004108207390840/cumchalice.gif");
   }
+
+  if (message.content.startsWith(config.prefix + "ruletag")) {
+    message.channel.send("Here you are dear, this is my RuleTag from RuleConnect34! https://cdn.discordapp.com/attachments/699626439074381946/700368169746825256/unknown.png ");
+  }
+
+  if (message.content.startsWith(config.prefix + "六四事件")) {
+    message.channel.send("除了人们一起吃蛋糕外，什么都没发生。 谈话足够，去古拉格。");
+  }
+
+  if (message.content.startsWith(config.prefix + "bugger")) {
+  if(message.mentions.members.size == 1) {
+let member = message.mentions.members.first()
+    var buggers = [`${message.author} buggered ${member}! Buggery is fantastic! https://i.makeagif.com/media/11-06-2016/a3Y0Q4.gif`, `${message.author} buggered ${member} and gave them an Arse Injected Death Sentence (AIDS)! Buggery can be fatal! https://tenor.com/view/roblox-porn-minecraft-hump-humping-gif-15545205`, `${message.author} buggered ${member}! Buggery is good for you! https://tenor.com/view/shrek-shrek-is-love-gif-5573099`];
+    var bugger = Math.floor(Math.random() * buggers.length);
+    message.channel.send(buggers[bugger]);
+  }
+}
 });
 
 client.login(config.token);
